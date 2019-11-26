1b7cc4886c16bfbe0dde5ade2c22e137ad190576
Phil Lembo <phil.lembo@gmail.com>
2019-11-26 16:56:05 -0500

    Way to get stats #1: increment variables

--- sof-demo.py
+++ sof-demo.py
@@ -4,5 +4,14 @@ import csv
 with open('data/survey_results_public.csv') as f:
     csv_reader = csv.DictReader(f)
 
+    yes_count = 0
+    no_count = 0
+
     for line in csv_reader:
-        print(line['Hobbyist'])
+        if line['Hobbyist'] == 'Yes':
+            yes_count += 1
+        elif line['Hobbyist'] == 'No':
+            no_count += 1
+
+print(yes_count)
+print(no_count)
