b1c8c2f4a6cd93cd2dbc932312911a07574f14c4
Phil Lembo <phil.lembo@gmail.com>
2019-11-26 17:06:13 -0500

    Use dictionary with Yes and No keys instead of separate variables

--- sof-demo.py
+++ sof-demo.py
@@ -4,20 +4,19 @@ import csv
 with open('data/survey_results_public.csv') as f:
     csv_reader = csv.DictReader(f)
 
-    yes_count = 0
-    no_count = 0
+    counts = {
+        'Yes': 0,
+        'No': 0
+    }
 
     for line in csv_reader:
-        if line['Hobbyist'] == 'Yes':
-            yes_count += 1
-        elif line['Hobbyist'] == 'No':
-            no_count += 1
+        counts[line['Hobbyist']] += 1
 
-total = yes_count + no_count
+total = counts['Yes'] + counts['No']
 
-yes_pct = (yes_count / total) * 100
+yes_pct = (counts['Yes'] / total) * 100
 yes_pct = round(yes_pct, 2)
-no_pct = (no_count / total) * 100
+no_pct = (counts['No'] / total) * 100
 no_pct = round(no_pct, 2)
 
 print(f'Yes: {yes_pct}%')
