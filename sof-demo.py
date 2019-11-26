df7aedfd811b4cb894df1c74ef9016dc06f639ac
Phil Lembo <phil.lembo@gmail.com>
2019-11-26 16:51:02 -0500

    Get response to Hobbyist question

--- sof-demo.py
+++ sof-demo.py
@@ -5,5 +5,5 @@ with open('data/survey_results_public.csv') as f:
     csv_reader = csv.DictReader(f)
 
     for line in csv_reader:
-        print(line)
+        print(line['Hobbyist'])
         break
